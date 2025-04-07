776fb6e8a92356b8c313d340b038eada19a8c32d
Tushar <kolhetushar7955@gmail.com>
2025-04-07 17:03:38 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,64 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT =
+  "I am passionate Fullstack Developer with a knack for crafting robust and scalable web appications. With 1 year and 5 months of hands on experience, I have honed my skills in front-end technologies like React.js and Next.js, as wll as backend technologies like Node.js, Express.js and database like MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.";
+
+export const ABOUT_TEXT =
+  "I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. 1 year and 5 months of hands on experience, I have worked with a variety of technologies, including React, Next.js, Node.js, Express.js, Postman, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.";
+
+export const EXPERIENCES = [
+  {
+    year: "Dec 2023 - Sep 2024",
+    role: "Software Developer (Intern)",
+    company: "Aeron Systems Pvt. Ltd",
+    description: `Worked on navigation and IoT systems for drones, robots, armored vehicles, and aircraft. Developed a unified App using React.js, JavaFX, HTML, and CSS to monitor multiple connected devices. Led dynamic device detection and interface integration for four systems, improving usability and adaptability. Collaborated in a 5-member team and resolved key system bugs.`,
+    technologies: ["Javascript", "React.js", "CSS", "JavaFx"],
+  },
+  // {
+  //   year: "2022 - 2023",
+  //   role: "Frontend Developer",
+  //   company: "Adobe",
+  //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+  //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kolhewadi, Pune-024, Maharashtara ",
+  phoneNo: "+91 8446531573 ",
+  email: "kolhetushar7955@gmail.com",
+};
